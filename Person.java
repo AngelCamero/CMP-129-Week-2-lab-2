01983f4c4594eae01f33d2c7227be3e5469d3278
AngelCamero <camero.angel@student.ccm.edu>
2026-09-15 17:48:42 -0400

    Finished person.java Starting persontest

--- Person.java
+++ Person.java
@@ -45,7 +45,12 @@ public class Person {
         p1.setName("Micheal Colon");
         p1.setEmail("mc42@gmail.com");
         p1.setAge(50);
-        System.out.println(p1);
-        
+        System.out.println(p1.displayInfo());
+
+        Person p2 = new Person();
+        p2.setName("mizuki shiraboshi");
+        p2.setEmail("Mizukis55@gmail.com");
+        p2.setAge(25);
+        System.out.print(p2.displayInfo());
     }
 }
