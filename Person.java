72e3910e69b14e5655ab139d2e157424a252ee20
AngelCamero <camero.angel@student.ccm.edu>
2026-09-15 16:47:17 -0400

    Almost finished with Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,51 @@
 public class Person {
+    private String name;
+    private String email;
+    private int age;
+
+    public Person() {
+        this.name = "N/A";
+        this.email = "N/A";
+        this.age = 0;
+    }
+
+    public Person(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
+    public String displayInfo() {
+        return "Name: " + this.name + "" +", Age: " + this.age +  ", Email: " + this.email;
+        
+
+    }
+    public static void main(String [] args){
+        Person p1 = new Person();
+        p1.setName("Micheal Colon");
+        p1.setEmail("mc42@gmail.com");
+        p1.setAge(50);
+        System.out.println(p1);
+        
+    }
 }
